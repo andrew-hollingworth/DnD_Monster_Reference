85c74f142d067efe820d51a5ac7a9bf64ab5bf3c
Andrew Hollingworth <andrewhollingworth@Andrews-MacBook-Air.local>
2019-08-29 09:46:03 -0400

    Updates

---
+++ precache-manifest.2875b26c11dbe9fea0537a06d1c12034.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21e6225ed020eff144cc9aa357c47541",
+    "url": "/DnD_Monster_Reference/index.html"
+  },
+  {
+    "revision": "b7be782506a4ed3c47ec",
+    "url": "/DnD_Monster_Reference/static/css/main.db3b1db4.chunk.css"
+  },
+  {
+    "revision": "142f14d52b3c8338d250",
+    "url": "/DnD_Monster_Reference/static/js/2.6827107c.chunk.js"
+  },
+  {
+    "revision": "b7be782506a4ed3c47ec",
+    "url": "/DnD_Monster_Reference/static/js/main.006504e1.chunk.js"
+  },
+  {
+    "revision": "3f6db7e9e53830c96bab",
+    "url": "/DnD_Monster_Reference/static/js/runtime~main.08bf1199.js"
+  },
+  {
+    "revision": "ece55f30d1e57d5909c0cfdf1b57b5b4",
+    "url": "/DnD_Monster_Reference/static/media/Mr Eaves Small Caps.ece55f30.otf"
+  },
+  {
+    "revision": "e3f3c6957440986c5f03cf9bf57b27cf",
+    "url": "/DnD_Monster_Reference/static/media/Nodesto Caps Condensed.e3f3c695.otf"
+  },
+  {
+    "revision": "f7de6ecd2c1f24a3f0342d3f0994f890",
+    "url": "/DnD_Monster_Reference/static/media/Scaly Sans Bold Italic.f7de6ecd.otf"
+  },
+  {
+    "revision": "ae64ea5b6a667b1973878139a1b78072",
+    "url": "/DnD_Monster_Reference/static/media/Scaly Sans Bold.ae64ea5b.otf"
+  },
+  {
+    "revision": "0b739c9ebf817630b69a5807a2049cea",
+    "url": "/DnD_Monster_Reference/static/media/Scaly Sans Italic.0b739c9e.otf"
+  },
+  {
+    "revision": "e0363d2ea6736f7d78a21c33fac277e5",
+    "url": "/DnD_Monster_Reference/static/media/Scaly Sans.e0363d2e.otf"
+  },
+  {
+    "revision": "3cedf8f91cb2dc5a1219ebc32e215462",
+    "url": "/DnD_Monster_Reference/static/media/Zatanna Misdirection.3cedf8f9.otf"
+  },
+  {
+    "revision": "05dbc5e3c2806d6d22d7c83e6869019b",
+    "url": "/DnD_Monster_Reference/static/media/bg-texture.05dbc5e3.jpg"
+  }
+]);
